488dbb81b2a133379513537a4bd73b39244f6e90
Andrew Nowak <andrew.nowak@guardian.co.uk>
2026-08-04 12:43:11 +0100

    update java skeleton

--- java/src/main/java/com/gu/App.java
+++ java/src/main/java/com/gu/App.java
@@ -2,12 +2,15 @@ package com.gu;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static String greet(String name) {
+        return "Hello " + name + "!";
     }
+
+    public static void main(String[] args) {
+        System.out.println(greet("World"));
+    }
+
 }
